4d4d1416ce7ed0f07c7acb2eb78c0dfb41411899
Pablo-source <datavizvalencia@gmail.com>
2025-01-26 17:24:56 +0000

    Created AE Type I Attendances bar plot

--- Matplotlib_gallery/03_barplot_using_AE_data.py
+++ Matplotlib_gallery/03_barplot_using_AE_data.py
@@ -19,7 +19,7 @@ AE_Attendances_file = r'/home/pablo/Documents/Pablo_zorin/VS_Python_GitHub_Pablo
 print(AE_Attendances_file)
 
 AE_data = pd.read_csv(AE_Attendances_file,
-parse_dates=[0],
+pxarse_dates=[0],
                       date_format = '%d/%m/%Y')  
 
 AE_data.head()
@@ -33,3 +33,40 @@ AE_data.head()
 AE_data.info()
 type(AE_data)
 
+# 5. Bar plots in matplotlib 
+
+# Subset data to keep just Type I Attendances 
+AE_TypeI = AE_data[['Date','Att_type1']]
+AE_TypeI.head()
+
+### 5.1 Subset data for specific dates
+AE_TypeI.set_index("Date",inplace=True)
+AE_TypeI.head()
+
+# 5.2 Now that my Date column is defined as an index,  I can use .loc for label based indexing or .iloc for positional indexing.
+# Subset  Aug, Sep, Oct 20210 data for this bar plot 
+AE_TypeI_AUG_OCT_2010 = AE_TypeI.loc['2010-08-01':'2010-10-01']
+AE_TypeI_AUG_OCT_2010
+
+### 5.3 Create bar chart for subset data
+# To create the bar chart I need to reset index, as I have setup Date as index
+# to slice my data 
+AE_TypeI_AUG_OCT_2010.head()
+AE_TypeI_AUG_OCT_2010_plot = AE_TypeI_AUG_OCT_2010.reset_index()
+AE_TypeI_AUG_OCT_2010_plot.head()
+
+# Now the data is ready to be used in matplotlib to create a bar plot
+
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+# We create a barplot using .bar() function 
+ax.bar(AE_TypeI_AUG_OCT_2010_plot.Date,
+       AE_TypeI_AUG_OCT_2010_plot.Att_type1)
+
+ax.set_ylabel('Count of TypeI attendances')
+ax.set_xlabel("Date")
+ax.set_title("Type I Attendances in England. Aug-Oct 2010")
+# Saving bar chart in plots sub-folder
+plt.savefig('Plots/Bar_plot_TypeI_Attendances_England_Aug_Oct_2010.png', bbox_inches='tight')
+plt.show()
