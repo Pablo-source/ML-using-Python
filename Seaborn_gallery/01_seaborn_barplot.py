307615d4bd09b191defea714380cf451ac8a8e79
Pablo-source <datavizvalencia@gmail.com>
2025-02-25 09:00:36 +0000

    writting python file for seaborn barchart

--- Seaborn_gallery/01_seaborn_barplot.py
+++ Seaborn_gallery/01_seaborn_barplot.py
@@ -19,3 +19,23 @@ os.chdir(new_wd_ML_using_python)
 
 new_wd = os.getcwd()
 print("Changed working directory to:",new_wd)
+
+
+# 3. Build path to Exel file to be imported from \data sub-folder
+data_folder = os.path.join("/home","pablo","Documents","Pablo_zorin","VS_Python_GitHub_Pablo_source","ML-using-Python",
+                           'data')
+data_folder_contents = os.listdir(data_folder)
+print('data folder contents:',data_folder_contents)
+
+# 3.1 Build  path to Excel file location
+ine_population_nationality = os.path.join('data','INE total and foreign population figures Spain.xlsx')
+print('ÍNE_population_nationality:',ine_population_nationality)
+
+# 4. Import Excel into Python
+
+# 4.1 Get tab names to choose which ones to import
+import openpyxl
+
+my_excel_file = pd.ExcelFile(ine_population_nationality)
+
+my_excel_file.sheet_names  # see all sheet names
