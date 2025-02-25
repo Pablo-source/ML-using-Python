1247b56ce11058a45280735a6a6e4973717b8597
Pablo-source <datavizvalencia@gmail.com>
2025-02-25 09:19:36 +0000

    Seaborn barplot python file completed

--- Seaborn_gallery/01_seaborn_barplot.py
+++ Seaborn_gallery/01_seaborn_barplot.py
@@ -77,9 +77,67 @@ INE_total_foreign_population.head()
 
 
 # 6. Subset variables and create Seaborn barplot 
-# Firs I want to subset just three columns from previous Data Frame: two main metrics
+# Subset just three columns from previous Data Frame: two main metrics
 # #  "Total_Population", "Foreign_population" and newly created "Year" column. Also when subsetting those columns
 # #  I want to place Year as the first column on the left.
 
 INE_foreign_pop_plot = INE_total_foreign_population[['Year','Total_population','Foreign_population']]
 INE_foreign_pop_plot.head()
+
+# 7. Create first Seaborn barplot 
+
+# 7.1 Sort data in ascending order for bar plot
+INE_first_plot = INE_foreign_pop_plot.head(21)
+INE_first_plot
+
+INE_first_plot.sort_values(by='Year',ascending = True) # Sort dataframe ascending order by Year column
+
+INE_first_plot.head() 
+
+
+# 6.1 First Unformatted Seaborn bar plot
+sns.set_theme()
+
+plt.figure(figsize=(3, 3))
+plt.rcParams["figure.figsize"] = (25, 15)
+sns.barplot(data = INE_first_plot_sorted,
+            x = "Year", 
+            y = "Total_population",log = False).set_title("Spanith total population.2005-2025 period")
+plt.show()
+
+# 7.2 Seaborn barplot formatted plot
+# Apply format to Y axis numbers to avoid scientific default notation
+from matplotlib.ticker import ScalarFormatter
+
+sns.set_theme()
+fig = plt.figure(figsize=(30, 15))
+axs = fig.add_subplot(1, 1, 1)
+sns.barplot(data = INE_first_plot_sorted,
+            x = "Year", 
+            y = "Total_population").set_title("Spanith total population.2005-2025 period")
+formatter = ScalarFormatter()
+formatter.set_scientific(False)
+axs.yaxis.set_major_formatter(formatter)
+plt.show()
+
+# 7.2 Save seaborn barplot as png file 
+
+# Save figure as Seaborn barplot, this is an example of a Seaborn bar plot with y axis formatted
+# Also the output plot will be saved as .png file in the Seaborn_plots sub-folder
+# Output: .png image saved inside Seaborn_gallery/Seaborn_plots folder
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+
+sns.set_theme()
+fig = plt.figure(figsize=(30, 15))
+axs = fig.add_subplot(1, 1, 1)
+sns.barplot(data = INE_first_plot_sorted,
+            x = "Year", 
+            y = "Total_population").set_title("Spanith total population.2005-2025 period")
+formatter = ScalarFormatter()
+formatter.set_scientific(False)
+axs.yaxis.set_major_formatter(formatter)
+plt.savefig('Seaborn_gallery/Seaborn_plots/Spanish total population 2005-2025 period.png', bbox_inches='tight') ## This saves the output plot successfully in the project folder
+plt.show()
