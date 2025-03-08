67e9c9579ff71eb18edba92c0d7c08f16face0b5
Pablo-source <datavizvalencia@gmail.com>
2025-03-08 18:02:41 +0000

    percent barplot as py file imported Excel file into python

--- Seaborn_gallery/02_percent_barplot_population_nationality.py
+++ Seaborn_gallery/02_percent_barplot_population_nationality.py
@@ -27,6 +27,7 @@ os.chdir(new_wd_ML_using_python)
 new_wd = os.getcwd()
 print("Changed working directory to:",new_wd)
 
+
 # 2. Build path to Exel file to be imported from \data sub-folder
 
 # - First we check the contents in our data folder to find Excel file to import into Python
@@ -41,3 +42,26 @@ print('data folder contents:',data_folder_contents)
 ine_population_nationality = os.path.join('data','INE total and foreign population figures Spain.xlsx')
 print('ÍNE_population_nationality:',ine_population_nationality)
 
+
+# 3. Import Excel file into Python 
+# Then we build path to Excel file we will load in Python using Pandas 
+# Get individual tab names from previous "INE total and foreign population figures Spain.xlsx" file
+
+my_excel_file = pd.ExcelFile(ine_population_nationality)
+
+my_excel_file.sheet_names  # see all sheet names
+
+INEdata = pd.read_excel(ine_population_nationality,
+                                  sheet_name = 'INE_Total_foreign_population',
+                                  skiprows= 2
+                                )
+INEdata.head()
+
+#  Keep only first 20 rows to skip footnotes
+INE_data_clean = INEdata.head(20)
+
+#  Rename columns
+INE_data_clean.columns = ['Date','Total_population','Foreign_population','Percent_foreign_population',
+'Total population YoY(N)','Total population  YoY(%)','Foreign population YoY(N)','Foreign population  YoY(%)']
+
+# 4. Create new calculated fields 
