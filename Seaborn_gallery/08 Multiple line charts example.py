82ae6d206b6cc3469ca4cdeb2e765f187ca23fbb
Pablo-source <datavizvalencia@gmail.com>
2025-05-16 09:36:29 +0100

    multiple line charts example

---
+++ Seaborn_gallery/08 Multiple line charts example.py
@@ -0,0 +1,26 @@
+# Load required libraries
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+# Seaborn libraries
+import seaborn as sns
+sns.set_theme(style = "darkgrid")
+
+num_rows = 20
+years = list(range(1900, 1900 + num_rows))
+
+data_wide = pd.DataFrame(
+    {
+        'Year':years,
+        'A': np.random.randn(num_rows).cumsum(),
+        'B': np.random.randn(num_rows).cumsum(),
+        'C': np.random.randn(num_rows).cumsum(),
+        'D': np.random.randn(num_rows).cumsum(),
+    }
+)
+
+data_wide.info()
+data_wide.head()
