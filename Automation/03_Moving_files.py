f9c536920e372dd85ee42209d18b2c6100b2a8b6
Pablo-source <datavizvalencia@gmail.com>
2025-12-02 10:14:13 +0000

    Created two folders for tesrting moving  shutil files

--- Automation/03_Moving_files.py
+++ Automation/03_Moving_files.py
@@ -1,3 +1,5 @@
+# Script: 03_Moving_files.py
+
 import os
 from os import path
 import glob
@@ -21,13 +23,13 @@ print(wd)
 print(f'working on the cloned folder:{wd}')
 
 # 1. Create new testing folder called "Test_files_A" and "Test_files_B"
+# Inside existing /Python_github_projects fodler
+def testing_folders_setup():
+    if not path.exists("Test_files_A"):
+        os.mkdir("Test_files_A")    
+    if not path.exists("Test_files_B"):
+        os.mkdir("Test_files_B")
+    
+    print("Created new testing folders")
 
-# def testing_folders_setup():
-#    if not path.exists("Test_files_A"):
-#        os.mkdir("Test_files_A")    
-#    if not path.exists("Test_files_B"):
-#        os.mkdir("Test_files_B")
-#    
-#    print("Created new testing folders")
-
-# testing_folders_setup()
+testing_folders_setup()
