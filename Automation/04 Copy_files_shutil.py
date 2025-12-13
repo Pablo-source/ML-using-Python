504761935366654eed434ff997678d5ba4beb6b6
Pablo-source <datavizvlc@gmail.com>
2025-12-13 11:45:00 +0100

    display current project folder structure

--- Automation/04 Copy_files_shutil.py
+++ Automation/04 Copy_files_shutil.py
@@ -6,3 +6,12 @@ import glob
 import shutil
 
 print(f"Script to practise how to copy files using functions")
+
+# Current WD
+wd = os.getcwd()
+print(wd)
+print(f'working on the cloned folder:{wd}')
+
+# Project directories
+folders_list = os.listdir()
+print(f'Current folder structure:{folders_list}')
