b82aafa3ce5b40f8880985a2058290498df1651a
Pablo-source <datavizvlc@gmail.com>
2025-12-13 11:55:37 +0100

    Created new folder to transfer files

--- Automation/04 Copy_files_shutil.py
+++ Automation/04 Copy_files_shutil.py
@@ -15,3 +15,14 @@ print(f'working on the cloned folder:{wd}')
 # Project directories
 folders_list = os.listdir()
 print(f'Current folder structure:{folders_list}')
+
+# Create two new folders to Copy files: 
+# "Test_folder_A" and "Test_folder_B"
+def create_new_folders():
+    if not path.exists("Test_folder_A"):
+        os.mkdir("Test_folder_A")
+    if not path.exists("Test_folder_B"):
+        os.mkdir("Test_folder_B")
+    print("Created Test A and Test B origin destination folders!")
+# Execute function
+create_new_folders()
