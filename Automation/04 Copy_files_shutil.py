1f5d9e67f167e2232e85fd36ab246eee83767f33
Pablo-source <datavizvlc@gmail.com>
2025-12-13 22:56:42 +0100

    Check contents test file A

--- Automation/04 Copy_files_shutil.py
+++ Automation/04 Copy_files_shutil.py
@@ -32,6 +32,16 @@ create_new_folders()
 # I want to import .csv file "data/AE_Attendances_Aug2010_Mar_2025.csv"
 
 # data_folder = os.path.join('/home','pablo','Documents','Pablo_zorin','VS_Python_GitHub_Pablo_source','ML-using-Python','data')
+
+Path
+----
+C:\Users\pituf\OneDrive\Documentos\Pablo\DELL_python_github\ML-using-Python
+
+
+PS C:\Users\pituf\OneDrive\Documentos\Pablo\DELL_python_github\ML-using-Python>
+
+
+
 Test_folder_A = os.path.join('C:','Users','pituf','OneDrive','Documentos','Pablo','DELL_python_github','ML-using-Python')
 Test_folder_A_contents = os.listdir(Test_folder_A)
 
