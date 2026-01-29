29e98c356ae4a02720cc781465058e89e3e5684a
Pablo-source <datavizvalencia@gmail.com>
2026-01-29 08:49:53 +0000

    testing ifelse with dictionary script

--- Miscellaneous_scripts/01_optimise_ifelse_using_dictionaries.py
+++ Miscellaneous_scripts/01_optimise_ifelse_using_dictionaries.py
@@ -3,3 +3,16 @@
 
 food_item = input("Enter food item name:")
 
+food_items_dict = { "burger": 9.00, 
+                   "pizza": 7.35, 
+                   "juice":2.15, 
+                   "tofu":9.00}
+
+# The get() method returns the value of an item with the specified key
+# You can also specify a default value to return if the key is not found.
+
+def getPrice(food_item):
+    return food_items_dict.get(food_item, "item not found")
+
+print(getPrice(food_item))
+
